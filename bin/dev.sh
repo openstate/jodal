e94aecfc89c706b3ac19373e08610108f65ab55b
Breyten Ernsting <bje@dds.nl>
2020-10-13 17:57:23 +0200

    Add a script to start in dev mode easily.

---
+++ bin/dev.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker exec -it jodal_node_1 npm run dev
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'world2'
+		name: 'world3'
 	}
 });
 
