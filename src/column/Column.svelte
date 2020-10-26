f192729453b072c343e9d5cfaeb09164b51c91ad
Breyten Ernsting <bje@dds.nl>
2020-10-26 18:39:58 +0100

    Working poc for columns.

--- src/column/Column.svelte
+++ src/column/Column.svelte
@@ -1,45 +1,59 @@
-<section class="column-section">
-<div class="column">
-  <div calss="column-title">
-    <h1>{ name }</h1>
-  </div>
-  <div class="column-contents">
-  {#each entries as entry}
-  	<Entry entry={entry}/>
-  {/each}
-  </div>
-</div>
-</section>
-
 <script>
 import Entry from './Entry.svelte';
+import VirtualList from '@sveltejs/svelte-virtual-list';
 
 export let name;
-let entries = [
+let start=0;
+let end=5;
+
+let items = [
   {
+    'key': "_1",
     'title': 'Vergadering 26-10-2020'
   },
   {
+    'key': "_2",
     'title': 'Vergadering 26-09-2020'
   },
   {
+    'key': "_3",
     'title': 'Vergadering 26-08-2020'
   },
   {
+    'key': "_4",
     'title': 'Vergadering 26-07-2020'
   },
   {
+    'key': "_5",
     'title': 'Vergadering 26-06-2020'
   },
   {
+    'key': "_6",
     'title': 'Vergadering 26-05-2020'
   },
   {
+    'key': "_7",
     'title': 'Vergadering 26-04-2020'
   }
-]
+];
 let empty = false;
+
+console.dir(items);
 </script>
+
+<section class="column-section">
+<div class="column">
+  <div class="column-title">
+    <h1>{ name }</h1>
+  </div>
+  <div class="column-contents">
+  <VirtualList {items} bind:start bind:end let:item>
+  	<Entry {...item}/>
+  </VirtualList>
+  <p>showing items {start}-{end}</p>
+  </div>
+</div>
+</section>
 
 <style>
 </style>
--- src/column/Entry.svelte
+++ src/column/Entry.svelte
@@ -1,6 +1,12 @@
-<div class="entry">
+<script>
+export let title;
+export let key;
+let empty = false;
+</script>
+
+<div class="entry" id="entry{key}">
   <div class="entry-title">
-    <h2>{ entry.title }</h2>
+    <h2>{ title }</h2>
   </div>
   <div class="entry-contents">
   Lorem ipsum Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit
@@ -9,10 +15,6 @@
   </div>
 </div>
 
-<script>
-export let entry;
-let empty = false;
-</script>
 
 <style>
 </style>
