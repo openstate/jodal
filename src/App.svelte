4040a8e3f5a3ee3de853af57de37cc4b675242a6
Breyten Ernsting <bje@dds.nl>
2020-11-04 17:41:12 +0100

    Add a fetc her component.

--- src/App.svelte
+++ src/App.svelte
@@ -6,12 +6,14 @@
 <Menu/>
 <Drawer/>
 <Workspace/>
+<Fetcher/>
 </div>
 
 <script>
 	import Menu from './Menu.svelte';
 	import Drawer from './Drawer.svelte';
   import Workspace from './Workspace.svelte';
+  import Fetcher from './Fetcher.svelte';
 </script>
 
 <style>
---
+++ src/Fetcher.svelte
@@ -0,0 +1,18 @@
+<script>
+import { onMount, onDestroy } from 'svelte';
+
+var interval;
+
+onMount(function () {
+  async function fetchData() {
+   console.log('should fetch data now!!');
+  }
+
+  interval = setInterval(fetchData, 5000);
+  fetchData();
+});
+
+onDestroy(function () {
+  clearInterval(interval);
+});
+</script>
