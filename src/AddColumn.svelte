415c17b9c6c2067308b6f57f6f96b68521453fcb
Breyten Ernsting <bje@dds.nl>
2020-11-12 15:30:23 +0100

    Start of dialog to dd a column.

---
+++ src/AddColumn.svelte
@@ -0,0 +1,34 @@
+<Dialog bind:this={simpleDialog} aria-labelledby="simple-title" aria-describedby="simple-content">
+ <!-- Title cannot contain leading whitespace due to mdc-typography-baseline-top() -->
+ <Title id="simple-title">Dialog Title</Title>
+ <Content id="simple-content">
+   Super awesome dialog body text?
+ </Content>
+ <Actions>
+   <Button on:click={() => clicked = 'No'}>
+     <Label>No</Label>
+   </Button>
+   <Button on:click={() => clicked = 'Yes'}>
+     <Label>Yes</Label>
+   </Button>
+ </Actions>
+</Dialog>
+
+<script context="module">
+  let simpleDialog;
+
+	export function startAddColumn() {
+    simpleDialog.open();
+	}
+</script>
+
+<script>
+  import Dialog, {Title, Content, Actions, InitialFocus} from '@smui/dialog';
+  import Button, {Label} from '@smui/button';
+  import List, {Item, Graphic, Text} from '@smui/list';
+  import Radio from '@smui/radio';
+  import Slider from '@smui/slider';
+  import FormField from '@smui/form-field';
+
+  let clicked = false;
+</script>
--- src/Menu.svelte
+++ src/Menu.svelte
@@ -10,16 +10,16 @@
             <Title>Jodal</Title>
           </Section>
           <Section align="end" toolbar>
-            <IconButton class="material-icons" aria-label="Download">file_download</IconButton>
-            <IconButton class="material-icons" aria-label="Print this page">print</IconButton>
-            <IconButton class="material-icons" aria-label="Bookmark this page">bookmark</IconButton>
+            <IconButton class="material-icons" aria-label="Add a column" on:click={() => startAddColumn()}>add</IconButton>
           </Section>
         </Row>
       </TopAppBar>
 </section>
 
+<AddColumn/>
 <script>
   import { drawerOpen } from './stores.js';
+  import AddColumn, { startAddColumn } from './AddColumn.svelte';
   import TopAppBar, {Row, Section, Title} from '@smui/top-app-bar';
   import IconButton from '@smui/icon-button';
   import Checkbox from '@smui/checkbox';
