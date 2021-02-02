4cc934fb9b04f111098294b99baffdad053fb226
Breyten Ernsting <bje@dds.nl>
2021-02-02 14:14:22 +0100

    Add mysql container + update docker config. Fixes #5

---
+++ docker/docker-entrypoint-initdb.d/binoas.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS `jodal`;
+
+-- CREATE TABLE IF NOT EXISTS `jodal`.`user` (
+--   `id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--   `application` VARCHAR(255) NOT NULL,
+--   `email` VARCHAR(255) NOT NULL,
+--   unique index idx_application_email (application, email)
+-- );
