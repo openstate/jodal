77e419531bce07452e1e666337ba0453c487e392
Breyten Ernsting <bje@dds.nl>
2021-02-17 16:40:49 +0100

    Show different buttons when logged in with titles.

--- src/Fetcher.svelte
+++ src/Fetcher.svelte
@@ -3,23 +3,11 @@ import { onMount, onDestroy } from 'svelte';
 import { get } from 'svelte/store';
 import { identity } from './stores.js';
 
-var url = 'http://api.jodal.nl/users/simple/me';
-fetch(
-  url,{credentials: "include"}).then(
-    response => response.json()
-  ).then(
-    function (data) {
-      console.log('Got identity with cookies in main:');
-      console.dir(data);
-      identity.set(data);
-    }
-  );
-
 onMount(function () {
   async function fetchData() {
     var url = 'http://api.jodal.nl/users/simple/me';
     return fetch(
-      url, {credentials: "same-origin"}).then(
+      url, {credentials: "include"}).then(
         response => response.json()
       ).then(
         function (data) {
--- src/Menu.svelte
+++ src/Menu.svelte
@@ -10,8 +10,13 @@
             <Title>Jodal</Title>
           </Section>
           <Section align="end" toolbar>
-            <IconButton class="material-icons" aria-label="Login" href="http://api.jodal.nl/users/simple/login">login</IconButton>
-            <IconButton class="material-icons" aria-label="Add a column" on:click={() => startAddColumn()}>add</IconButton>
+          {#if $identity}
+            <IconButton class="material-icons" aria-label="Add a column" title="Kolom toevoegen" on:click={() => startAddColumn()}>add</IconButton>
+            <IconButton class="material-icons" aria-label="Account" title="{$identity.email}">account_circle</IconButton>
+            <IconButton class="material-icons" aria-label="Logout" href="http://api.jodal.nl/users/simple/logout" title="Uitloggen">logout</IconButton>
+          {:else}
+            <IconButton class="material-icons" aria-label="Login" href="http://api.jodal.nl/users/simple/login" title="Inloggen">login</IconButton>
+          {/if}
           </Section>
         </Row>
       </TopAppBar>
@@ -19,7 +24,7 @@
 
 <AddColumn/>
 <script>
-  import { drawerOpen,fetchingEnabled } from './stores.js';
+  import { drawerOpen,fetchingEnabled, identity } from './stores.js';
   import AddColumn, { startAddColumn } from './AddColumn.svelte';
   import TopAppBar, {Row, Section, Title} from '@smui/top-app-bar';
   import IconButton from '@smui/icon-button';
