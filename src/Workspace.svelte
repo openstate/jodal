79927088ff56a469015448ccf2d40e99439f3641
Breyten Ernsting <bje@dds.nl>
2021-03-10 16:28:47 +0100

    Show instructions to add a column if there are none present. Fixes #18

--- src/Workspace.svelte
+++ src/Workspace.svelte
@@ -1,5 +1,11 @@
 <div class="workspace" id="workspace">
   <div class="columns">
+  {#if $inquiries.length <= 0}
+  <div class="start-explainer">
+    <p>Je hebt nog geen kolommen toegevoegd. Klik op de knop hieronder om er een toe te voegen.</p>
+    <Button align="end" on:click={() => startAddColumn()}><Label>Kolom toevoegen</Label></Button>
+  </div>
+  {/if}
     {#each $inquiries as inq}
     	<Column inquiry={inq} />
     {/each}
@@ -9,7 +15,11 @@
 <script>
 import { inquiries } from './stores.js';
 import Column from './column/Column.svelte';
-let empty = false;
+import AddColumn, { startAddColumn } from './AddColumn.svelte';
+import Button from '@smui/button';
+import Fab, {Label, Icon} from '@smui/fab';
+
+let empty = ($inquiries.length <= 0);
 </script>
 
 <style>
