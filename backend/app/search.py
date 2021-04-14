279099ef67a40a08df8693c5ebb24144e521e35b
Breyten Ernsting <bje@dds.nl>
2021-04-14 12:38:32 +0200

    Make aggregations also configurable per indesx. See #24

--- backend/app/search.py
+++ backend/app/search.py
@@ -7,7 +7,6 @@ from app import app, AppError, load_object
 app_name = app.config['NAME_OF_APP']
 setup_es = load_object('%s.es.setup_elasticsearch' % (app_name,))
 es = setup_es(app.config[app_name])
-aggregation_fields = app.config[app_name]['elasticsearch']['aggregations']
 
 def perform_query(term, filter_string, page, page_size, sort, index_name=None):
 
@@ -16,10 +15,13 @@ def perform_query(term, filter_string, page, page_size, sort, index_name=None):
     if index_name is None:
         # TODO: query fields adjustable per index, or by query somehow
         query_fields = app.config[app_name]['elasticsearch'].get('query', {}).get('fields', [])
+        aggregation_fields = app.config[app_name]['elasticsearch']['aggregations']
     else:
         idx = '_'.join(index_name.split('_')[1:])
         query_fields = app.config[app_name]['elasticsearch'].get('indices', {}).get(idx, {}).get('query', {}).get('fields', [])
-    query = get_basic_query(filters, term, page, page_size, sort, query_fields)
+        aggregation_fields = app.config[app_name]['elasticsearch'].get('indices', {}).get(idx, {})['aggregations']
+
+    query = get_basic_query(filters, term, page, page_size, sort, query_fields, aggregation_fields)
     logging.info(query)
     result = es.search(index=index_name, body=query)
 
@@ -38,7 +40,7 @@ def parse_filters(filters):
 
 
 #Those queries implement the filters as AND filter and the query as query_string_query
-def get_basic_query(filters, term, page, page_size, sort, query_fields):
+def get_basic_query(filters, term, page, page_size, sort, query_fields, aggregation_fields):
         query = None
 
         if not page:
