aa75a41c9c0c0e211b4dd4525685441c7356d0a8
Breyten Ernsting <bje@dds.nl>
2021-09-17 15:01:56 +0200

    A bit of styling for the dialog.

--- src/Help.svelte
+++ src/Help.svelte
@@ -1,8 +1,15 @@
+<style>
+  .acc-header {
+    width: 100%;
+  }
+</style>
+
 <Dialog
   bind:open
   bind:this={helpDialog}
   aria-labelledby="default-focus-title"
   aria-describedby="default-focus-content"
+  class="help-dialog"
 >
   <Title id="default-focus-title">Help</Title>
   <Content id="default-focus-content">
@@ -11,7 +18,10 @@
   <Accordions>
 
      <Accordion id="1" {openedAccordion} on:accordionSelected={toggleAccordion}>
-     <span slot="title" class="valign-wrapper"><i class="material-icons">place</i>First</span>
+     <span slot="title" class="valign-wrapper">
+      Weten waar JODAL voor te gebruiken is?
+      <span class="material-icons acc-chevron">chevron_right</span>
+     </span>
      <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
      tempor incididunt ut labore et dolore magna aliqua.
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -19,7 +29,10 @@
     </Accordion>
 
     <Accordion  id="2" {openedAccordion}  on:accordionSelected={toggleAccordion}>
-      <span slot="title" class="valign-wrapper"><i class="material-icons">place</i>Second</span>
+      <span slot="title" class="valign-wrapper">
+        Een account aanmaken op JODAL
+        <span class="material-icons acc-chevron">chevron_right</span>
+      </span>
       <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua.
        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -27,7 +40,10 @@
     </Accordion>
 
     <Accordion id="3" {openedAccordion}  on:accordionSelected={toggleAccordion}>
-      <span slot="title" class="valign-wrapper"><i class="material-icons">whatshot</i>Third</span>
+      <span slot="title" class="valign-wrapper">
+        Uitleg over de hoofdpagina van JODAL
+        <span class="material-icons acc-chevron">chevron_right</span>
+      </span>
       <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
       incididunt ut labore et dolore magna aliqua.
        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
@@ -69,9 +85,3 @@
     helpDialog.open();
 	}
 </script>
-
-<style>
-  .acc-header {
-    width: 100%;
-  }
-</style>
