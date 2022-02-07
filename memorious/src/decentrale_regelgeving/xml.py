5ffb555696a1e4a00a32513e8291fb9114c24062
Breyten Ernsting <bje@dds.nl>
2022-02-07 18:24:33 +0100

    Better mapping and add municipalities data.

--- memorious/src/decentrale_regelgeving/xml.py
+++ memorious/src/decentrale_regelgeving/xml.py
@@ -16,6 +16,59 @@ def _prefix_tag(ns, tag, start='./'):
     else:
         return '%s%s' % (start, tag)
 
+def crawl_simple(context, data):
+    # This stage comes after 'fetch' so the 'data' input contains an
+    # HTTPResponse object.
+    response = context.http.rehash(data)
+    url = response.url
+    page = response.xml
+    total_elem = page.find(_prefix_tag('srw', 'numberOfRecords'))
+
+    context.log.info(total_elem)
+    if total_elem is not None:
+        total_count = int(total_elem.text)
+        matches = re.search('&startRecord=(\d+)', url)
+        if matches is not None:
+            current_count = int(matches.group(1))
+            # if (current_count + 10) < total_count:
+            if (current_count + 10) < 30:  # total_count:
+                next_url = url.replace(
+                    '&startRecord='+str(current_count),
+                    '&startRecord='+str(current_count+10))
+                context.log.info('Yielding url: %s', next_url)
+                context.emit(rule="fetch", data={"url": next_url})
+    # Parse the rest of the page to extract structured data.
+    context.log.info('Finding records: %s', _prefix_tag('srw', 'record', './/'))
+    for record in page.findall(_prefix_tag('srw', 'record', './/')):
+        #context.log.info('Found record! %s', record)
+        record_data = {
+            #"content_hash":
+            'id': record.find(_prefix_tag('dcterms', 'identifier', './/')).text,
+            "title": record.find(_prefix_tag('dcterms', 'title', './/')).text,
+            "author": '%s %s' % (
+                record.find(_prefix_tag('sru', 'organisatietype', './/')).text,
+                record.find(_prefix_tag('dcterms', 'creator', './/')).text,),
+            "url": record.find(_prefix_tag('sru', 'publicatieurl_xhtml', './/')).text,
+            'retrieved_at': record.find(_prefix_tag('dcterms', 'modified', './/')).text,
+            'modified_at': record.find(_prefix_tag('dcterms', 'modified', './/')).text,
+            'published_at': record.find(_prefix_tag('dcterms', 'issued', './/')).text,
+            'request_id': record.find(_prefix_tag('dcterms', 'identifier', './/')).text,
+            'mime_type': 'text/html',
+            'countries': ["nl"],
+            'languages': ["nl"],
+            'keywords': [r.text for r in record.findall(_prefix_tag('dcterms', 'subject', './/'))]
+            # "author": quote.find('.//small[@class="author"]').text_content(),
+            # "tags": ", ".join(
+            #     [tag.text_content() for tag in quote.findall('.//a[@class="tag"]')]
+            # ),  # noqa
+        }
+        context.log.info('Parsed result: %s', record_data)
+        context.emit(rule="fetch", data=record_data)
+        # If 'rule' is not set, it defaults to 'pass', which triggers the
+        # final 'store' stage.
+        context.emit(data=record_data)
+    #context.emit(rule="cleanup", data={"content_hash": response.content_hash})
+    context.emit(data=data)
 
 def crawl(context, data):
     # This stage comes after 'fetch' so the 'data' input contains an
@@ -44,6 +97,7 @@ def crawl(context, data):
         #context.log.info('Found record! %s', record)
         record_data = {
             #"content_hash":
+            'id': record.find(_prefix_tag('dcterms', 'identifier', './/')).text,
             "title": record.find(_prefix_tag('dcterms', 'title', './/')).text,
             "author": '%s %s' % (
                 record.find(_prefix_tag('sru', 'organisatietype', './/')).text,
