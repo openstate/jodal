6423d6cb27b151ea2d333740e44acb69fd441649
Breyten Ernsting <bje@dds.nl>
2022-02-08 17:29:00 +0100

    Upload documents.

--- memorious/src/load_decentrale_regelgeving.py
+++ memorious/src/load_decentrale_regelgeving.py
@@ -1,15 +1,24 @@
 from glob import glob
+from os.path import basename, splitext
+import json
+from pathlib import Path
 
 # Import alephclient:
 from alephclient.api import AlephAPI
+
+def load_meta(f):
+    result = None
+    with open(f, 'r') as in_file:
+        result = json.load(in_file)
+    return result
 
 # By default, alephclient will read host and API key from the
 # environment. You can also pass both as an argument here:
 api = AlephAPI()
 
 # Get the collection (dataset)
-foreign_id = 'decentrale_regelgeving_xml'
-data_path = '/data/results/decentrale_regelgeving'
+foreign_id = 'decentrale_regelgeving'
+data_path = '/data/results/%s' % (foreign_id)
 collection = api.load_collection_by_foreign_id(foreign_id)
 collection_id = collection.get('id')
 
@@ -18,11 +27,26 @@ for e in entities:
     print(e)
 
 for f in glob('%s/*.json' % (data_path,)):
-    print(f)
-# file_path = 'www.personadeinteres.org/uploads/example.pdf'
-# metadata = {'file_name': 'example.pdf'}
-# # Upload the document:
-# result = api.ingest_upload(collection_id, file_path, metadata)
-#
-# # Finally, we have an entity ID:
-# document_id = result.get('id')
+    # print(f)
+    root, ext = splitext(f)
+    print(root, ext)
+    meta = load_meta(f)
+    print(meta)
+    if 'id' not in meta.keys():
+        print("No id for crawled result, continuing")
+        continue
+    html_file = '%s.data.html' % (root,)
+    metadata = {
+        'file_name': '%s.html' % (meta['id'],)
+    }
+    print(html_file)
+    result = api.ingest_upload(collection_id, Path(html_file), metadata)
+    document_id = result.get('id')
+    print(document_id)
+    # file_path = 'www.personadeinteres.org/uploads/example.pdf'
+    # metadata = {'file_name': 'example.pdf'}
+    # # Upload the document:
+    # result = api.ingest_upload(collection_id, file_path, metadata)
+    #
+    # # Finally, we have an entity ID:
+    # document_id = result.get('id')
