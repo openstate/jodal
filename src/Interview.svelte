084684b15a68e3f8eff276ad0137b2b4c7296a5f
Breyten Ernsting <bje@dds.nl>
2022-08-17 15:59:56 +0200

    Embed wufoo form.

--- src/Interview.svelte
+++ src/Interview.svelte
@@ -4,11 +4,9 @@ import { fade, slide } from 'svelte/transition';
 </script>
 <section class="column-section" out:fade>
 <div id="column-interview" class="column">
-  <div class="column-title">
-    <h2>Enquete</h2>
-  </div>
   <div id="column-contents-interview" class="column-contents">
-  Hier willen we interview vragen.
+    <div id="wufoo-form-loading" class="loading"></div>
+    <iframe height="799" onload="document.getElementById('wufoo-form-loading').style.display='none';" title="Embedded Wufoo Form" allowtransparency="true" frameborder="0" scrolling="no" style="width:100%;border:none" sandbox="allow-top-navigation allow-scripts allow-popups allow-forms allow-same-origin" src="https://jodal.wufoo.com/embed/zekl9wu16ytpmk/"> <a href="https://jodal.wufoo.com/forms/zekl9wu16ytpmk/">Fill out my Wufoo form!</a> </iframe>
   </div>
 </div>
 </section>
