3972138b92983c5a30597f576f484402f051daaf
Breyten Ernsting <bje@dds.nl>
2023-01-31 13:45:46 +0100

    Change url.

--- src/stores.js
+++ src/stores.js
@@ -1,7 +1,7 @@
 import { readable, writable, get, derived } from 'svelte/store';
 import { onMount } from "svelte";
 
-export const domainName = 'politiekdatadashboard.nl';
+export const domainName = 'jodal.nl';
 export const apiDomainName = 'api.' + domainName;
 export const testDomainName = 'test.' + domainName;
 
