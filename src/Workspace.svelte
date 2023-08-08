c42efa2d0512deb6b7ac29cb7e1ba3ae0f49f040
Breyten Ernsting <bje@dds.nl>
2023-08-08 17:42:26 +0200

    Remove things we do not need anymore.

--- src/Workspace.svelte
+++ src/Workspace.svelte
@@ -8,25 +8,11 @@
   {/if}
   {#if $identity || $isTesting}
     <Message/>
-    {#if $inquiries.length <= 0}
-    <section class="column-section">
-    <div id="column-contents-new-inq" class="column-contents">
-      <p>Je hebt nog geen zoekopdracht toegevoegd. Klik op de knop hieronder om er een toe te voegen.</p>
-      <Button align="end" on:click={() => startAddColumn()}><Label>Zoekopdracht toevoegen</Label></Button>
-    </div>
-    </section>
-    {/if}
     {#each $ordered_inquiries as inq}
       {#if inq.id == $selected_inquiry_id}
     	<Column inquiry={inq} />
       {/if}
     {/each}
-  {:else}
-  <div class="start-explainer">
-    <p>Je bent niet ingelogd. Klik op de &eacute;&eacute;n van de knoppen hieronder om in te loggen of een account aan te maken.</p>
-    <Button align="end" href="//www.{domainName}/login/"><Label>Inloggen</Label></Button> of
-    <Button align="end" href="//www.{domainName}/register/"><Label>Registreren</Label></Button>
-  </div>
   {/if}
   </div>
 </div>
