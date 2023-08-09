9d54822950a07304bfeb680d3419d71afe296fb9
Breyten Ernsting <bje@dds.nl>
2023-08-09 12:00:45 +0200

    Add fields to subscribe dialog.

--- src/Subscribe.svelte
+++ src/Subscribe.svelte
@@ -6,7 +6,21 @@
 >
   <Title id="default-focus-title">Abboneer</Title>
   <Content id="default-focus-content">
-    input email hiero
+    <div>
+      <Label>E-mail</Label>
+      <Textfield>
+        <Input bind:value={email} id="subscribe-email" />
+      </Textfield>
+    </div>
+    <div>
+      <Label>Frequentie</Label>
+      <Select bind:value={frequency}>
+        <Option value="direct" selected={frequency == 'direct'}>Onmiddelijk</Option>
+        <Option value="hourly" selected={frequency == 'hourly'}>Elk uur</Option>
+        <Option value="daily" selected={frequency == 'daily'}>Elke dag</Option>
+        <Option value="weekly" selected={frequency == 'weekly'}>Elke week</Option>
+      </Select>
+    </div>
   </Content>
   <Actions>
     <Button
@@ -22,9 +36,18 @@
 <script>
   import Dialog, { Title, Content, Actions, InitialFocus } from '@smui/dialog';
   import Button, { Label } from '@smui/button';
+  import Textfield, {Input, Textarea} from '@smui/textfield';
+  import FloatingLabel from '@smui/floating-label';
+  import LineRipple from '@smui/line-ripple';
+  import Select, {Option} from '@smui/select';
+  import { identity } from './stores.js';
 
   let open;
   let response = 'Nothing yet.';
+  let email = '';
+  let frequency = 'direct';
+
+  $: if ($identity) { email=$identity.email }
 </script>
 
 <script context="module">
