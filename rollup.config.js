a26e55d2ea826a4e85643d5d6412e955a0fdc3cb
Breyten Ernsting <bje@dds.nl>
2023-09-06 16:17:17 +0200

    Add server bits (not working yet).

--- rollup.config.js
+++ rollup.config.js
@@ -39,6 +39,7 @@ export default {
 	},
 	plugins: [
 		svelte({
+			hydratable: true,
 			// enable run-time checks when not in production
 			dev: !production,
 			// we'll extract any component CSS out into
