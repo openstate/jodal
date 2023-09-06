1e1a9ef6a62c894620aaedeb2e5e68e431f75502
Breyten Ernsting <bje@dds.nl>
2023-09-06 16:07:57 +0200

    Do not forget ot include pages.

---
+++ src/pages/Bronnen.svelte
@@ -0,0 +1,8 @@
+<script>
+    let pageName="Bronnen";
+</script>
+
+<main>
+    <h1> {pageName}!</h1>
+    <p>Welcome this is my <b>{pageName}</b></p>
+</main>
---
+++ src/pages/Over.svelte
@@ -0,0 +1,8 @@
+<script>
+    let pageName="Over";
+</script>
+
+<main>
+    <h1> {pageName}!</h1>
+    <p>Welcome this is my <b>{pageName}</b></p>
+</main>
