513f0c49138ff798faeaaca745d877bef11fa55d
Breyten Ernsting <bje@dds.nl>
2024-01-16 18:14:30 +0100

    Add a route to convert a document. See #145

--- texter/app/main.py
+++ texter/app/main.py
@@ -1,9 +1,32 @@
-from flask import Flask
+import json
+import tempfile
+from io import BytesIO
+
+from flask import Flask, session, render_template, request, redirect, url_for, flash, Markup, jsonify, send_file
+
+import requests
+import fitz
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World from Flask"
+
+@app.route("/convert")
+def convert():
+    url = request.args.get('url', '')
+    filetype = request.args.get('filetype', 'pdf')
+    text = ''
+    resp = requests.get(url)
+    doc=fitz.open(stream=BytesIO(resp.content), filetype=filetype)
+    for page in doc: # iterate the document pages
+        text += "\n\n" + page.get_text() # get plain text encoded as UTF-8
+    return json.dumps({
+        'url': url,
+        'filetype': filetype,
+        'text': text
+    })
 
 if __name__ == "__main__":
     # Only for debugging while developing
