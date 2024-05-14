46020a10543f656259e143658bb992381d4917b3
Breyten Ernsting <bje@dds.nl>
2024-05-14 12:06:59 +0200

    Add more herixtrix requests. See #169

--- backend/app/archive.py
+++ backend/app/archive.py
@@ -9,12 +9,23 @@ HERITRIX_URL = 'https://heritrix:8443/engine'
 HERITRIX_USER = 'jodal'
 HERITRIX_PASSWD = 'jodal'
 
-def heritrix_request(params={}):
+def heritrix_request(path='', params={}):
+    url = f"{HERITRIX_URL}"
+    if path != '':
+        url += f"/{path}"
     return requests.post(
-        HERITRIX_URL,
+        url,
         data=params,
         auth=HTTPDigestAuth(HERITRIX_USER, HERITRIX_PASSWD),
         verify=False)
+
+def heritrix_job_create(job_id):
+    resp = heritrix_request({'action': 'create', 'createpath': job_id})
+    return resp
+
+def heritrix_job_status(job_id):
+    resp = heritrix_request(f"job/{job_id}")
+    return resp
 
 def get_warc_archive_id(user_id, url):
     identifier = f"{user_id}\{url}"
@@ -26,7 +37,8 @@ def warc_create_archive(url, user):
     result = {}
     # make identifier base on url + user_id
     hash_id = get_warc_archive_id(user['id'], url)
-    # TODO: create new job : https://heritrix.readthedocs.io/en/latest/api.html#create-new-job
+    # create new job : https://heritrix.readthedocs.io/en/latest/api.html#create-new-job
+    resp = heritrix_job_create(hash_id)
     # TODO: build job configuration : https://heritrix.readthedocs.io/en/latest/api.html#build-job-configuration
     # TODO: launch job : https://heritrix.readthedocs.io/en/latest/api.html#launch-job
     return result
