f7106e5362ec80a2e7da37de92f7898a6ee95442
Breyten Ernsting <bje@dds.nl>
2024-05-22 12:03:29 +0200

    Add login dialog skeleton. See #177

---
+++ src/Login.svelte
@@ -0,0 +1,20 @@
+<h1>Inloggen</h1>
+
+<script>
+import { onMount, onDestroy } from 'svelte';
+import { showLoginDialog } from './LoginDialog.svelte';
+
+export let id = "";
+
+$: if(id != "") { id="gelukt"; /*showLoginDialog()*/ }
+
+onMount(function () {
+  console.log('login onMount');
+  id = "test";
+  showLoginDialog();
+});
+
+onDestroy(function () {
+});
+
+</script>
---
+++ src/LoginDialog.svelte
@@ -0,0 +1,37 @@
+<Dialog
+  bind:open
+  bind:this={loginDialogObj}
+  aria-labelledby="default-focus-title"
+  aria-describedby="default-focus-content"
+>
+  <Title id="default-focus-title">Inloggen</Title>
+  <Content id="default-focus-content">
+  <input  class="input-full-width input-full-height" bind:value={emailAddress} id="forgot-email" />
+  </Content>
+  <Actions>
+    <Button
+      default
+      use={[InitialFocus]}
+      on:click={() => (response = 'It will be glorious.')}
+    >
+      <Label>Inloggen</Label>
+    </Button>
+  </Actions>
+</Dialog>
+
+<script>
+  import Dialog, { Title, Content, Actions, InitialFocus } from '@smui/dialog';
+  import Button, { Label } from '@smui/button';
+
+  let open;
+  let response = 'Nothing yet.';
+  let emailAddress = "";
+</script>
+
+<script context="module">
+  let loginDialogObj;
+
+	export function showLoginDialog() {
+    loginDialogObj.open();
+	}
+</script>
