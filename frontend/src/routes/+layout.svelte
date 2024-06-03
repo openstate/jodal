6ffe4f9c743c1937201d1b55f1ae123e31c4d4ec
Breyten Ernsting <bje@dds.nl>
2024-06-03 18:30:05 +0200

    Import tabler in the app. See #169

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from "svelte";
 	import "../scss/variables.scss";
 	import "../scss/bootstrap.scss";
+	import "../scss/tabler.scss";
 	import "../scss/app.scss";
 	import { browser } from "$app/environment";
 	import NavBar from '$lib/NavBar.svelte';
@@ -11,6 +12,7 @@
 	onMount(async () => {
 		if (!browser) return;
 		await import("bootstrap");
+		await import("@tabler/core");
 	});
 </script>
 
