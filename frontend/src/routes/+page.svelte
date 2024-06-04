9b8c36a0a395e18d0b24351c70b73988de2d67a8
Breyten Ernsting <bje@dds.nl>
2024-06-04 14:54:09 +0200

    Working stuff for Tabler and Svelte. See #169

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,64 +2,133 @@
   import { Mastodon, TwitterX, Linkedin, Github, EnvelopeFill } from 'svelte-bootstrap-icons';
   import OSFLogo from '$lib/assets/logo-openstate.png';
   import exampleLogo from '$lib/assets/example-logo.png';
+  import { IconHeart } from '@tabler/icons-svelte';
+  let input = 'xxx';
 </script>
 
 <div class="page">
-  <header class="navbar navbar-expand-sm navbar-light d-print-none">
-    <div class="container-xl">
-      <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
+  <!-- Sidebar -->
+  <aside class="navbar navbar-vertical navbar-expand-sm navbar-dark">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <h1 class="navbar-brand navbar-brand-autodark">
         <a href="#">
-          <img src="{OSFLogo}" width="110" height="32" alt="Tabler" class="navbar-brand-image" />
+          <img src="{OSFLogo}" width="110" height="32" alt="Tabler" class="navbar-brand-image">
         </a>
       </h1>
-
-      <div class="navbar-nav flex-row order-md-last">
-        <div class="nav-item">
-          <a href="#" class="nav-link d-flex lh-1 text-reset p-0">
-            <span class="avatar avatar-sm" style="background-image: url(...)"></span>
-            <div class="d-none d-xl-block ps-2">
-              <div>Paweł Kuna</div>
-              <div class="mt-1 small text-secondary">UI Designer</div>
-            </div>
-          </a>
+      <div class="collapse navbar-collapse" id="sidebar-menu">
+        <ul class="navbar-nav pt-lg-3">
+          <li class="nav-item">
+            <a class="nav-link" href="./">
+              <span class="nav-link-title">
+                Home
+              </span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span class="nav-link-title">
+                Link 1
+              </span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span class="nav-link-title">
+                Link 2
+              </span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span class="nav-link-title">
+                Link 3
+              </span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </aside>
+  <div class="page-wrapper">
+    <div class="page-header d-print-none">
+      <div class="container-xl">
+        <div class="row g-2 align-items-center">
+          <div class="col">
+            <h2 class="page-title">
+              Vertical layout
+            </h2>
+          </div>
         </div>
       </div>
     </div>
-  </header>
-
-
-  <div class="page-wrapper">
     <div class="page-body">
       <div class="container-xl">
         <div class="row row-deck row-cards">
-          <div class="col-4">
+          <div class="col-sm-6 col-lg-3">
             <div class="card">
               <div class="card-body" style="height: 10rem">
-
-              <div class="dropdown">
-                <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown">
-                  <!-- SVG icon from http://tabler-icons.io/i/calendar -->
-                  <svg>...</svg>
-                </button>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="#">
-                    Action
-                  </a>
-                  <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </div>
-              </div>
-
+                <a href="#" class="btn" role="button" on:click="{() => input='blaat'}">Update</a>
               </div>
             </div>
           </div>
-          <div class="col-4">
+          <div class="col-sm-6 col-lg-3">
+            <div class="card">
+              <div class="card-body" style="height: 10rem">{input}</div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-3">
             <div class="card">
               <div class="card-body" style="height: 10rem"></div>
             </div>
           </div>
-          <div class="col-4">
+          <div class="col-sm-6 col-lg-3">
+            <div class="card">
+              <div class="card-body" style="height: 10rem"></div>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="row row-cards">
+              <div class="col-12">
+                <div class="card">
+                  <div class="card-body" style="height: 10rem"></div>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="card">
+                  <div class="card-body" style="height: 10rem"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="card">
+              <div class="card-body" style="height: 10rem"></div>
+            </div>
+          </div>
+          <div class="col-12">
+            <div class="card">
+              <div class="card-body" style="height: 10rem"></div>
+            </div>
+          </div>
+          <div class="col-md-12 col-lg-8">
+            <div class="card">
+              <div class="card-body" style="height: 10rem"></div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <div class="card">
+              <div class="card-body" style="height: 10rem"></div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <div class="card">
+              <div class="card-body" style="height: 10rem"></div>
+            </div>
+          </div>
+          <div class="col-md-12 col-lg-8">
             <div class="card">
               <div class="card-body" style="height: 10rem"></div>
             </div>
