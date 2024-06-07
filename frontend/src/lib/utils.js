6742856d514c1c938f04eef7b9b5ff740f0b37b7
Breyten Ernsting <bje@dds.nl>
2024-06-07 17:14:39 +0200

    Request the identity so we can know if someone was logged in. See #169

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,6 +1,27 @@
+import { identity } from '$lib/stores';
+
 export function initBronApp() {
   getIdentity();
 }
 export function getIdentity() {
   console.log('should do identity check now!');
+  var apiDomainName = 'api.bron.live';
+  var url = '//' + apiDomainName + '/users/simple/me';
+  return fetch(
+    url, {credentials: "include", cache: 'no-cache'}).then(
+      response => response.json()
+    ).then(
+      function (data) {
+        if (data) {
+          console.log('Identity: logged in as ', data);
+          //console.dir(data);
+          identity.set(data);
+          console.log('Setting if testing');
+          isTesting.set(typeof(data) !== 'object');
+        } else {
+          console.log('Identity: not logged in')
+        }
+      }
+    );
+
 }
