6c4b61f5cbb478763c0eb2cb3915128a305f38e4
Breyten Ernsting <bje@dds.nl>
2024-07-22 12:17:12 +0200

    Change config so the frontend node will auto start in dev mode and run properly in production mode. See #169

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+
+	server: {
+		fs: {
+			allow: [
+				'static'
+			]
+		}
+	}
+
 });
