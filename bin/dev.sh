3ea329b628bc7acae8bd5a23cb4e4304261e06bd
coretteket <33397397+Coretteket@users.noreply.github.com>
2024-11-12 11:50:02 +0100

    `docker-compose` -> `docker compose`

--- bin/dev.sh
+++ bin/dev.sh
@@ -2,7 +2,7 @@
 FQPATH=`readlink -f $0`
 BINDIR=`dirname $FQPATH`
 cd $BINDIR/../docker
-docker-compose -f docker-compose.yml -f docker-compose-dev.yml up -d
+docker compose -f docker-compose.yml -f docker-compose-dev.yml up -d
 #sleep 5
 #docker exec -it jodal_node_1 npm run dev
 cd -
