576cdd9a6f1847fb28e0e7f462bc39e869f847f1
coretteket <33397397+Coretteket@users.noreply.github.com>
2024-12-04 10:20:01 +0100

    Functional search and examples on landing page

--- frontend/src/lib/examples.ts
+++ frontend/src/lib/examples.ts
@@ -1,29 +1,131 @@
 const examples = [
-  { name: "Windmolens in Eemshaven", emoji: "🌬️" },
-  { name: "Sociale huur in Rotterdam", emoji: "🏠" },
-  { name: "Onderwijs in Utrecht", emoji: "📚" },
-  { name: "Vergrijzing in Limburg", emoji: "🧓" },
-  { name: "Fietsbeleid in Amsterdam", emoji: "🚲" },
-  { name: "Waterveiligheid in Zeeland", emoji: "🌊" },
-  { name: "Monumenten in Leiden", emoji: "🏛️" },
-  { name: "Toegankelijkheid", emoji: "♿" },
-  { name: "Bosbeheer in Gelderland", emoji: "🌳" },
-  { name: "Toerisme op Texel", emoji: "🏖️" },
-  { name: "Cultuur in Maastricht", emoji: "🎭" },
-  { name: "Cybercriminaliteit", emoji: "🛡️" },
-  { name: "Luchtkwaliteit", emoji: "🌫️" },
-  { name: "Zwerfafval", emoji: "🗑️" },
-  { name: "Digitalisering", emoji: "💻" },
-  { name: "Verkeersveiligheid", emoji: "🚦" },
-  { name: "Warmtevisie", emoji: "🌡️" },
-  { name: "Transparantie", emoji: "🔍" },
-  { name: "Diversiteit", emoji: "🌈" },
-  { name: "Gezondheid", emoji: "🏥" },
-  { name: "Energietransitie", emoji: "🔋" },
-  { name: "Woningbouw", emoji: "🏗️" },
-  { name: "Mobiliteit", emoji: "🚗" },
-  { name: "Werkgelegenheid", emoji: "👩‍💼" },
-  { name: "Veiligheid", emoji: "🦺" },
+  {
+    name: "Windmolens in Noord-Holland",
+    emoji: "🌬️",
+    query: "windmolens&organisaties=PV27",
+  },
+  {
+    name: "Sociale huur in Rotterdam",
+    query: '"sociale huur"&organisaties=GM0599',
+    emoji: "🏠",
+  },
+  {
+    name: "Onderwijs in Utrecht",
+    query: "onderwijs&organisaties=GM0344",
+    emoji: "📚",
+  },
+  {
+    name: "Vergrijzing in Limburg",
+    query: "vergrijzing&organisaties=PV31",
+    emoji: "🧓",
+  },
+  {
+    name: "Fietsbeleid in Amsterdam",
+    query:
+      "fiets | fietsers | fietsen&organisaties=GM0363,GM0363S56,GM0363S90,GM0363S10,GM0363S88,GM0363S91,GM0363S89,GM0363S17",
+    emoji: "🚲",
+  },
+  {
+    name: "Waterveiligheid in Zeeland",
+    query: "waterveiligheid | overstroming | dijk | kering&organisaties=PV29",
+    emoji: "🌊",
+  },
+
+  {
+    name: "Monumenten in Leiden",
+    query: "monumenten&organisaties=GM0546",
+    emoji: "🏛️",
+  },
+  {
+    name: "Bosbeheer in Gelderland",
+    query: "bosbeheer | bos | bossen | bomen&organisaties=PV25",
+    emoji: "🌳",
+  },
+  {
+    name: "Toerisme op Texel",
+    query: "toerisme | toeristen&organisaties=GM0448",
+    emoji: "🏖️",
+  },
+  {
+    name: "Cultuur in Maastricht",
+    query: "cultuur&organisaties=GM0935",
+    emoji: "🎭",
+  },
+  {
+    name: "Toegankelijkheid",
+    query: "toegankelijkheid",
+    emoji: "♿",
+  },
+  {
+    name: "Cybercriminaliteit",
+    query: "cybercrime | cybercriminaliteit | cybersecurity",
+    emoji: "🛡️",
+  },
+  {
+    name: "Luchtkwaliteit",
+    query: 'luchtkwaliteit | "schone lucht"',
+    emoji: "🌫️",
+  },
+  {
+    name: "Zwerfafval",
+    query: "zwerfafval",
+    emoji: "🗑️",
+  },
+  {
+    name: "Digitalisering",
+    query: "digitalisering | ICT | cloud",
+    emoji: "💻",
+  },
+  {
+    name: "Verkeersveiligheid",
+    query: "verkeersveiligheid",
+    emoji: "🚦",
+  },
+  {
+    name: "Warmtevisie",
+    query: "warmtevisie",
+    emoji: "🌡️",
+  },
+  {
+    name: "Transparantie",
+    query: "transparantie | geheimhouding | WOO",
+    emoji: "🔍",
+  },
+  {
+    name: "Diversiteit",
+    query: "diversiteit | inclusiviteit",
+    emoji: "🌈",
+  },
+  {
+    name: "Gezondheid",
+    query: "zorg | gezondheid | ziekenhuis | huisarts",
+    emoji: "🏥",
+  },
+  {
+    name: "Energietransitie",
+    query: "energietransitie",
+    emoji: "🔋",
+  },
+  {
+    name: "Woningbouw",
+    query: "woningbouw",
+    emoji: "🏗️",
+  },
+  {
+    name: "Mobiliteit",
+    query: "mobiliteit",
+    emoji: "🚗",
+  },
+  {
+    name: "Werkgelegenheid",
+    query: "werkgelegenheid | banen | werk",
+    emoji: "👩‍💼",
+  },
+  {
+    name: "Veiligheid",
+    query: "veiligheid | veilig",
+    emoji: "🦺",
+  },
 ];
 
 function pickRandom<T>(array: T[], n: number): T[] {
--- frontend/src/routes/(site)/home/+page.svelte
+++ frontend/src/routes/(site)/home/+page.svelte
@@ -5,17 +5,19 @@
 </script>
 
 <section class="max-w-300 mx-auto">
-  <h1 class="pt-10 font-display text-center text-6xl font-bold">
+  <h1 class="font-display pt-10 text-center text-6xl font-bold">
     Superkrachten voor journalisten
   </h1>
   <h2 class="mt-8 text-center text-2xl font-light text-stone-700">
     Onderzoek 13 miljoen overheidsdocumenten op één plek
   </h2>
-  <div
+  <form
+    action="/zoeken"
     class="max-w-150 mx-auto mt-16 flex items-center justify-between rounded-lg border border-stone-300 bg-white text-lg focus-within:border-stone-400"
   >
     <input
       type="search"
+      name="zoek"
       placeholder="Zoek over elk onderwerp..."
       class="w-full px-6 py-4 text-xl placeholder:text-stone-400 focus:outline-0"
     />
@@ -24,17 +26,18 @@
     >
       <ArrowRight />
     </button>
-  </div>
+  </form>
   <div
     class="max-w-150 max-h-22 mx-auto mt-6 flex flex-wrap justify-center gap-2 overflow-y-hidden"
   >
     {#each data.examples as example}
-      <div
+      <a
+        href="/zoeken?zoek={example.query}"
         class="flex h-10 items-center gap-2 rounded-lg border border-stone-300 bg-white px-3 py-2 font-light text-stone-700"
       >
         <span>{example.emoji}</span>
         {example.name}
-      </div>
+      </a>
     {/each}
   </div>
 </section>
