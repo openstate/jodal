3ca7d5269690b5fa7e2dceffd0e65b56411052c5
coretteket <33397397+Coretteket@users.noreply.github.com>
2024-12-09 17:21:56 +0100

    Add navigation icons and feeds in layout

--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -4,6 +4,13 @@
 
   import { enhance } from "$app/forms";
 
+  import Search from "@tabler/icons-svelte/icons/search";
+  import Home_2 from "@tabler/icons-svelte/icons/home-2";
+  import Planet from "@tabler/icons-svelte/icons/planet";
+  import MessageCircle from "@tabler/icons-svelte/icons/message-circle";
+  import ExternalLink from "@tabler/icons-svelte/icons/external-link";
+  import LayersSubtract from "@tabler/icons-svelte/icons/layers-subtract";
+
   let { data, children } = $props();
 </script>
 
@@ -14,7 +21,7 @@
 
 {#snippet link(href: string, name: string)}
   <a
-    class="block rounded px-4 py-2 transition-colors hover:bg-stone-100"
+    class="block rounded px-4 py-2 font-medium transition-colors hover:bg-stone-100"
     {href}
   >
     {name}
@@ -27,15 +34,53 @@
       <a class="inline-block p-4" href="/">
         <img src={BronLogo} class="w-42" alt="Bron Logo" />
       </a>
-      {@render link("/", "Home")}
-      {@render link("/zoeken", "Zoeken")}
-      {@render link("/feeds", "Feeds")}
-      {#each data.feeds ?? [] as feed}
-        {@render link(
-          `/feeds/${feed.public_id}`,
-          `Feed '${feed.name}'`,
-        )}
-      {/each}
+      <a
+        href="/zoeken"
+        class="mx-2 my-2 flex cursor-pointer gap-2.5 rounded-lg border border-2 border-stone-200 bg-white px-3 py-2 text-stone-700 transition-colors hover:border-stone-300"
+      >
+        <Search class="w-4.5" />
+        Zoeken
+      </a>
+      <a
+        class="flex items-center gap-2.5 rounded px-4 py-2 transition-colors hover:bg-stone-100"
+        href="/"
+      >
+        <Home_2 class="w-5 text-stone-800" />
+        Home
+      </a>
+      <a
+        class="flex items-center gap-2.5 rounded px-4 py-2 transition-colors hover:bg-stone-100"
+        href="/"
+      >
+        <Planet class="w-5 text-stone-800" />
+        Ontdekken
+      </a>
+      <a
+        class="flex items-center gap-2.5 rounded px-4 py-2 transition-colors hover:bg-stone-100"
+        href="/feeds"
+      >
+        <LayersSubtract class="w-5 text-stone-800" />
+        Feeds
+      </a>
+      <div class="my-1 ml-6 border-l-2 border-l-stone-200 pl-2 text-stone-700">
+        {#each data.feeds ?? [] as feed}
+          <a
+            class="block rounded px-3 py-1 text-sm capitalize transition-colors hover:bg-stone-50"
+            href="/feeds/{feed.public_id}"
+          >
+            {feed.name}
+          </a>
+        {/each}
+      </div>
+      <a
+        class="flex items-center gap-2.5 rounded px-4 py-2 transition-colors hover:bg-stone-100"
+        href="//chat.bron.live"
+        target="_blank"
+      >
+        <MessageCircle class="w-5 text-stone-800" />
+        Chat
+        <ExternalLink class="ml-auto w-4 text-stone-500" />
+      </a>
     </div>
     <div>
       {#if data.identity}
