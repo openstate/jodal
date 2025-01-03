ae95a31f2c0654e609d54988b6452223f7fd3d2f
coretteket <33397397+Coretteket@users.noreply.github.com>
2025-01-03 16:04:49 +0100

    different mobile navigation (wip)

--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -1,13 +1,5 @@
 <script lang="ts">
-  import BronLogo from "$lib/assets/bron-logo.svg";
-  import LayersSubtract from "@tabler/icons-svelte/icons/layers-subtract";
-  import Sidebar from "./sidebar.svelte";
-  import Planet from "@tabler/icons-svelte/icons/planet";
-  import Home_2 from "@tabler/icons-svelte/icons/home-2";
-  import Search from "@tabler/icons-svelte/icons/search";
-  import MessageCircle from "@tabler/icons-svelte/icons/message-circle";
-  import { page } from "$app/state";
-  import Bookmarks from "@tabler/icons-svelte/icons/bookmarks";
+  import Navigation from "./navigation.svelte";
 
   let { data, children } = $props();
 </script>
@@ -17,50 +9,22 @@
   <meta property="og:title" content="Bron" />
 </svelte:head>
 
-<main class="grid h-screen">
-  <nav
-    class="flex items-center border-b-2 border-stone-200 bg-white px-2 [grid-area:topbar] sm:hidden"
-  >
-    <a class="inline-block p-4" href="/">
-      <img src={BronLogo} class="w-28" alt="Bron Logo" />
-    </a>
-  </nav>
-  <Sidebar {data} />
-  <article class="overflow-y-scroll p-4 [grid-area:content]">
+<main class="grid h-dvh">
+  <Navigation {data} />
+  <article class="overflow-y-scroll p-4">
     <div class="sm:max-w-300 sm:mx-auto">
       {@render children?.()}
     </div>
   </article>
-  <nav
-    class="grid grid-flow-col items-center justify-around border-t-2 border-stone-200 bg-white px-4 [grid-area:bottombar] sm:hidden"
-  >
-    <a class="flex items-center gap-2" href="/">
-      <Home_2 class="w-6 text-stone-800" />
-    </a>
-
-    <a class="flex items-center gap-2" href="/zoeken">
-      <Search class="w-6 text-stone-800" />
-    </a>
-    <a class="flex items-center gap-2" href="/feeds">
-      <Bookmarks class="w-6 text-stone-800" />
-    </a>
-    <a class="flex items-center gap-2" href="/">
-      <Planet class="w-6 text-stone-800" />
-    </a>
-  </nav>
 </main>
 
 <style>
-  @media (width < 40rem) {
-    main {
-      grid-template-areas: "topbar" "content" "bottombar";
-      grid-template-rows: 4rem 1fr 4rem;
+  main {
+    @media (width < 40rem) {
+      grid-template-rows: 4rem 1fr;
     }
-  }
 
-  @media (width >= 40rem) {
-    main {
-      grid-template-areas: "sidebar content";
+    @media (width >= 40rem) {
       grid-template-columns: 18rem 1fr;
     }
   }
--- frontend/src/routes/(app)/navigation.svelte
+++ frontend/src/routes/(app)/navigation.svelte
@@ -9,15 +9,102 @@
   import Planet from "@tabler/icons-svelte/icons/planet";
   import MessageCircle from "@tabler/icons-svelte/icons/message-circle";
   import ExternalLink from "@tabler/icons-svelte/icons/external-link";
-  import LayersSubtract from "@tabler/icons-svelte/icons/layers-subtract";
-    import Bookmarks from "@tabler/icons-svelte/icons/bookmarks";
+  import Bookmarks from "@tabler/icons-svelte/icons/bookmarks";
+  import Menu_2 from "@tabler/icons-svelte/icons/menu-2";
+  import Close from "@tabler/icons-svelte/icons/x";
 
   let { data } = $props();
 
   const MAX_SIDEBAR_FEEDS = 5;
+
+  let sidebarOpen = $state(false);
+
+  let menuClass = $derived(sidebarOpen ? "opacity-0 rotate-90" : "");
+  let closeClass = $derived(sidebarOpen ? "" : "opacity-0 -rotate-90");
 </script>
 
-<nav class="flex h-full flex-col border-r-2 border-stone-200 bg-white p-4 [grid-area:sidebar] max-sm:hidden">
+<!-- Mobile -->
+<nav
+  class="z-50 flex items-center justify-between border-b-2 border-stone-200 bg-white px-4 sm:hidden"
+>
+  <a class="inline-block" href="/">
+    <img src={BronLogo} class="w-30" alt="Bron Logo" />
+  </a>
+  <button
+    class="relative flex h-10 w-10 items-center justify-center"
+    onclick={() => (sidebarOpen = !sidebarOpen)}
+  >
+    <div class="absolute transition {menuClass}">
+      <Menu_2 class="size-7 text-stone-700" />
+    </div>
+    <div class="absolute transition {closeClass}">
+      <Close class="size-7 text-stone-700" />
+    </div>
+  </button>
+</nav>
+
+<div
+  class={[
+    "fixed left-0 top-16 z-40 w-full border-b-2 border-stone-200 bg-white p-4 transition sm:hidden",
+    !sidebarOpen && "-translate-y-full",
+  ]}
+>
+  <a
+    href="/zoeken"
+    class="mx-2 my-2 flex cursor-pointer gap-2.5 rounded-lg border-2 border-stone-200 bg-white px-3 py-2 text-stone-700 transition-colors hover:border-stone-300"
+    onclick={() => (sidebarOpen = false)}
+  >
+    <Search class="w-4.5" />
+    Zoeken
+  </a>
+  <a
+    class="mx-2 flex items-center gap-2.5 rounded px-2 py-2 transition-colors hover:bg-stone-100"
+    href="/"
+    onclick={() => (sidebarOpen = false)}
+  >
+    <Home_2 class="w-5 text-stone-800" />
+    Home
+  </a>
+  <a
+    class="mx-2 flex items-center gap-2.5 rounded px-2 py-2 transition-colors hover:bg-stone-100"
+    href="/"
+    onclick={() => (sidebarOpen = false)}
+  >
+    <Planet class="w-5 text-stone-800" />
+    Ontdekken
+  </a>
+  <a
+    class="mx-2 flex items-center gap-2.5 rounded px-2 py-2 transition-colors hover:bg-stone-100"
+    href="/feeds"
+    onclick={() => (sidebarOpen = false)}
+  >
+    <Bookmarks class="w-5 text-stone-800" />
+    Feeds
+  </a>
+
+  <a
+    class="mx-2 flex items-center gap-2.5 rounded px-2 py-2 transition-colors hover:bg-stone-100"
+    href="//chat.bron.live"
+    target="_blank"
+    onclick={() => (sidebarOpen = false)}
+  >
+    <MessageCircle class="w-5 text-stone-800" />
+    Chat
+    <ExternalLink class="ml-auto w-4 text-stone-500" />
+  </a>
+</div>
+
+<div
+  class={[
+    "fixed z-30 h-dvh w-dvw bg-black/50 transition",
+    sidebarOpen ? "" : "pointer-events-none opacity-0",
+  ]}
+></div>
+
+<!-- Desktop -->
+<nav
+  class="flex h-full flex-col border-r-2 border-stone-200 bg-white p-4 max-sm:hidden"
+>
   <div class="font-display grow">
     <a class="inline-block p-4" href="/">
       <img src={BronLogo} class="w-36" alt="Bron Logo" />
