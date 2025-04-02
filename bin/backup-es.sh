c686bf6c25ffbd1180c3e511e9d245399c58732b
coretteket <33397397+Coretteket@users.noreply.github.com>
2025-04-02 11:43:38 +0200

    fix elasticsearch backup script

--- bin/backup-es.sh
+++ bin/backup-es.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-ES_LATEST=`sudo docker exec jodal_backend_1 curl -s 'http://elasticsearch:9200/_snapshot/ood_backups/_all?pretty' |jq -r '.snapshots[-1] |.snapshot |@text'`
-sudo docker exec jodal_backend_1 curl -XPUT 'http://elasticsearch:9200/_snapshot/ood_backups/\%3Cbackup1-\%7Bnow\%2Fd\%7D\%3E'
-sudo docker exec jodal_backend_1 curl -XDELETE "http://elasticsearch:9200/_snapshot/ood_backups/$ES_LATEST"
+
+CONTAINER="jodal_backend_1"
+
+# Get the name of the latest backup
+ES_LATEST=`sudo docker exec $CONTAINER curl -s 'http://elasticsearch:9200/_snapshot/ood_backups/_all?pretty' |jq -r '.snapshots[-1] |.snapshot |@text'`
+
+# Create a name for the new backup
+SNAPSHOT_NAME="backup1_$(date +%Y%m%d%H%M)"
+
+# Create a new backup
+sudo docker exec $CONTAINER curl -XPUT "http://elasticsearch:9200/_snapshot/ood_backups/$SNAPSHOT_NAME?wait_for_completion=true"
+
+# Delete the latest backup
+sudo docker exec $CONTAINER curl -XDELETE "http://elasticsearch:9200/_snapshot/ood_backups/$ES_LATEST"
